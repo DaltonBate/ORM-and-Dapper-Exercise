ad6cb54f657fb538d73b0da3f6bc84d431f40bb9
Dalton Bate <dilton8763@gmail.com>
2024-06-19 18:10:25 -0400

    Finished excersise

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,62 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Drawing.Printing;
+
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Configuration
+            var config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            #endregion
+            IDbConnection conn = new MySqlConnection(connString);
+            DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
+
+            Console.WriteLine("Hello user, here are the present departments:");
+            Console.WriteLine("Please press enter.");
+            Console.ReadLine();
+
+            var depos = repo.GetAllDepartments();
+            
+
+            foreach (var depo in depos) 
+            { 
+               Console.WriteLine($"Id: {depo.DepartmentID} Name: {depo.Name}");
+            }
+
+            Console.WriteLine("Do you want to add a department?");
+
+            string userResponse = Console.ReadLine();
+
+            if (userResponse.ToLower() == "yes") 
+            {
+                Console.WriteLine("What is the name of your new department?");
+                userResponse = Console.ReadLine();
+               repo.InsertDepartment(userResponse);
+
+                repo.GetAllDepartments();
+                Print(repo.GetAllDepartments());
+            }
+
+            Console.WriteLine("Have a nice day.");
+        }
+
+        private static void Print(IEnumerable<Department> depos) 
+        {
+            foreach (var depo in depos)
+            {
+                Console.WriteLine($"Id: {depo.DepartmentID} Name: {depo.Name}");
+            }
         }
     }
 }
--- ORM-Dapper/obj/Debug/net6.0/ORM-Dapper.AssemblyInfo.cs
+++ ORM-Dapper/obj/Debug/net6.0/ORM-Dapper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c105c41a4ea95f76f119ac3f5c84bd31012c752")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d342546e08d6a328a0d194b3226b98e26f856164")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
